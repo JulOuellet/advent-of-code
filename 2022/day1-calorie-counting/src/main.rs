87ebdb9caaca0a9fadf6f78ec79acf2d1bc34976
JulOuellet <julien.ouellet@live.ca>
2023-10-27 19:56:42 -0400

    part one solution

--- 2022/day1-calorie-counting/src/main.rs
+++ 2022/day1-calorie-counting/src/main.rs
@@ -5,10 +5,33 @@ use std::{
 
 fn main() -> std::io::Result<()> {
 
+    let mut max_calories = 0;
+    let mut current_calories = 0;
+
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
+    
+    // iterate on lines
+    for line in reader.lines() {
+        let line = line?;
 
-    // TODO: iterate on reader and solve puzzle
+        // if current line is empty, we are done with the current elf
+        if line.is_empty() {
+
+            // if current calories is bigger than previous max, it becomes the new max
+            if current_calories > max_calories {
+                max_calories = current_calories;
+                current_calories = 0;
+            } else {
+                current_calories = 0;
+            }
+
+        } else {
+            current_calories += line.trim().parse().unwrap_or(0);
+        }
+    }
+
+    println!("{}", max_calories);
 
     Ok(())
 }
