93911a4635c19e796e5f153966ca2ebb8c15c586
JulOuellet <julien.ouellet@live.ca>
2023-10-27 20:46:46 -0400

    refactor part one

--- 2022/day1-calorie-counting/src/main.rs
+++ 2022/day1-calorie-counting/src/main.rs
@@ -1,38 +1,13 @@
-use std::{
-    fs::File, 
-    io::{BufRead, BufReader}
-};
+mod part_one;
+
+use std::{fs::File, io::BufReader};
+use crate::part_one::part_one_solution;
 
 fn main() -> std::io::Result<()> {
 
-    let mut max_calories = 0;
-    let mut current_calories = 0;
-
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
-    
-    // iterate on lines
-    for line in reader.lines() {
-        let line = line?;
 
-        // if current line is empty, we are done with the current elf
-        if line.is_empty() {
-
-            // if current calories is bigger than previous max, it becomes the new max
-            if current_calories > max_calories {
-                max_calories = current_calories;
-                current_calories = 0;
-            } else {
-                current_calories = 0;
-            }
-
-        } else {
-            current_calories += line.trim().parse().unwrap_or(0);
-        }
-    }
-
-    println!("{}", max_calories);
-
-    Ok(())
+    part_one_solution(reader)
 }
 
