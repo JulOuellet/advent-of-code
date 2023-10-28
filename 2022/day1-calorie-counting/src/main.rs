5e1a42d16e4a151cb28ea5c6e621be605c0653ee
JulOuellet <julien.ouellet@live.ca>
2023-10-28 12:23:13 -0400

    refactor part one

--- 2022/day1-calorie-counting/src/main.rs
+++ 2022/day1-calorie-counting/src/main.rs
@@ -7,43 +7,25 @@ fn main() -> std::io::Result<()> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
     
-    part_one_solution(reader)
-}
-
-fn part_one_solution(reader: BufReader<File>) -> std::io::Result<()> {
+    // part one 
     let mut max_calories = 0;
     let mut current_calories = 0;
 
-    // iterate on lines
-    for line in reader.lines() {
-        let line = line?;
-
-        // if current line is empty, we are done with the current elf
-        if line.is_empty() {
-
-            // if current calories is bigger than previous max, it becomes the new max
-            if current_calories > max_calories {
-                max_calories = current_calories;
+    reader.lines()
+        .map(|line| line.expect("Error reading line..."))
+        .for_each(|line| {
+            
+            // If line is empty, we have a new elf
+            if line.is_empty() {
+                if current_calories > max_calories {
+                    max_calories = current_calories;
+                }
                 current_calories = 0;
             } else {
-                current_calories = 0;
+                current_calories += line.trim().parse::<i32>().unwrap_or(0);
             }
-        } else {
-            current_calories += line.trim().parse().unwrap_or(0);
-        }
-    }
-
-    println!("Total calories carried by the elf carrying the most calories: {}", max_calories);
+        });
+    println!("The elf carrying the most calories is carrying {} calories!", max_calories);
     Ok(())
 }
 
-fn part_two_solution(reader: BufReader<File>) -> std::io::Result<()> {
-    let mut most_calories = 0;
-    let mut second_most_calories = 0;
-    let mut third_most_calories = 0;
-    let mut current_calories = 0;
-    
-    // iterate over lines
-
-    Ok(()) 
-}
