f31f8bdc82848678958bfcc81ac15d8b4b8355bd
JulOuellet <julien.ouellet@live.ca>
2023-10-29 16:13:08 -0400

    aoc 2022 day 1 completion

--- 2022/day1-calorie-counting/src/main.rs
+++ 2022/day1-calorie-counting/src/main.rs
@@ -1,13 +1,13 @@
 use std::{
     fs::File, 
-    io::{BufReader, BufRead}
+    io::{BufReader, BufRead, SeekFrom, Seek}
 };
 
 fn main() -> std::io::Result<()> {
     let file = File::open("input.txt")?;
 
     /* --- part one --- */
-    let reader1 = BufReader::new(file.try_clone()?);
+    let reader1 = BufReader::new(&file);
     let mut max_calories = 0;
     let mut current_calories = 0;
 
@@ -28,34 +28,33 @@ fn main() -> std::io::Result<()> {
     println!("The elf carrying the most calories is carrying {} calories!", max_calories);
 
     /* --- part two --- */
-    let reader2 = BufReader::new(file);
-    let mut max_calories : Vec<i32> = Vec::new();
-    let mut current_calories = 0;
+    let mut reader2 = BufReader::new(&file);
+    reader2.get_mut().seek(SeekFrom::Start(0));
 
+    let mut current_calories = 0;
+    let mut top_three: Vec<i32> = vec![0, 0, 0];
+    
     reader2.lines()
         .map(|line| line.expect("Error reading line..."))
         .for_each(|line| {
+            
             if line.is_empty() {
-                update_max_calories(&mut max_calories, current_calories);
-                current_calories = 0;
+                 check_top_three(current_calories, &mut top_three);
+                 current_calories = 0;
             } else {
                 current_calories += line.trim().parse::<i32>().unwrap_or(0);
             }
         });
-    println!(
-        "The three elfs carrying the most calories are carrying {} calories!",
-        max_calories.iter().sum::<i32>()
-    );
+    println!("The three elfs carrying the most calories are carrying {} calories!",
+             top_three.iter().sum::<i32>());
 
     Ok(())
 }
 
-fn update_max_calories(max_calories: &mut Vec<i32>, current_calories: i32) {
-    if current_calories > 0 {
-        max_calories.push(current_calories);
-        max_calories.sort_unstable_by(|a, b| b.cmp(a));
-        if max_calories.len() > 3 {
-            max_calories.pop();
-        }
+fn check_top_three(current_calories: i32, top_three_vec: &mut Vec<i32>) {
+    if current_calories > top_three_vec[0] {
+        top_three_vec[0] = current_calories;
+        top_three_vec.sort_unstable();
     }
 }
+
