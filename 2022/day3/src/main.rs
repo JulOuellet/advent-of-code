d66594db1c065953363c8ee77f4538dc12eb98b3
JulOuellet <julien.ouellet@live.ca>
2023-10-30 17:40:01 -0400

    day 3 boilerplate

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{BufReader, BufRead}
+};
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("input.txt")?;
+    let reader = BufReader::new(&file);
+
+    reader.lines()
+        .map(|line| line.expect("Error reading line..."))
+        .for_each(|line| {
+            // TODO solve problem
+        });
+    Ok(())
 }
+
