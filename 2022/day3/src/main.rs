b174db83baaa8c69e9007f038c1ebba337fd8ba1
JulOuellet <julien.ouellet@live.ca>
2023-11-02 23:39:00 -0400

    day 3 completion

--- 2022/day3/src/main.rs
+++ 2022/day3/src/main.rs
@@ -20,7 +20,21 @@ fn main() -> std::io::Result<()> {
     /* --- part two ---*/
     let mut reader2 = BufReader::new(&file);
     let _ = reader2.get_mut().seek(SeekFrom::Start(0));
+
+    let mut group = Vec::new();
     let mut total_score_p2 = 0;
+
+    reader2.lines()
+        .map(|line| line.expect("Error reading line..."))
+        .for_each(|line| {
+            group.push(line);
+
+            if group.len() == 3 {
+                total_score_p2 += get_score_p2(&group, &priorities);
+                group.clear();
+            }
+        });
+    println!("Sum of priorities for part two: {}", total_score_p2);
 
     Ok(())
 }
@@ -47,4 +61,27 @@ fn get_score_p1(line: &str, priorities: &HashMap<char, i32>) -> i32 {
 
     score
 }
+
+fn get_score_p2(group: &[String], priorities: &HashMap<char, i32>) -> i32 {
+    let mut score = 0;
+
+    let first_line: HashSet<_> = group[0].chars().collect();
+    let second_line: HashSet<_> = group[1].chars().collect();
+    let third_line: HashSet<_> = group[2].chars().collect();
+
+    let common_char_set: HashSet<char> = first_line
+        .intersection(&second_line)
+        .cloned()
+        .collect();
+
+    let common_char = common_char_set
+        .intersection(&third_line)
+        .next()
+        .expect("Could not fin char");
+
+    score += priorities.get(common_char).unwrap_or(&0);
+
+    score
+}
+
 
