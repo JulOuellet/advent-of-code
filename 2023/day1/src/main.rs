d645d9386893b9ba88ae0736be6d4e77ac87f42a
JulOuellet <julien.ouellet@live.ca>
2023-12-01 23:17:37 -0500

    complete part one

--- 2023/day1/src/main.rs
+++ 2023/day1/src/main.rs
@@ -3,48 +3,27 @@ use std::{
     io::{BufReader, BufRead}
 };
 
-fn main() {
-    let input = File::open("input.txt").unwrap();
+fn main() -> std::io::Result<()> {
+    let input = File::open("input.txt")?;
     let reader = BufReader::new(&input);
 
     let mut values: Vec<i32> = Vec::new();
 
-    reader.lines().for_each(|line| {
-        let line = line.unwrap();
-            
-        if line.is_empty() {
-            return;
-        }
+    reader.lines()
+        .map(|line| line.expect("Error reading line..."))
+        .filter(|line| !line.is_empty())
+        .for_each(|line| values.push(find_numbers(&line)));
 
-        values.push(find_numbers(&line));
+    println!("Sum of all calibration values: {}", values.iter().sum::<i32>());
 
-        println!("Line: {}", values[0]);
-    });
-
-    print!("Sum of all calibration values: {}", values.iter().sum::<i32>());
-
-
+    Ok(())
 }
 
 fn find_numbers(line: &str) -> i32 {
-    let mut first = 0;
-    let mut last = 0;
-    
-    for character in line.chars() {
-        if character.is_digit(10) {
-            first = character.to_digit(10).unwrap() as usize;
-            break;
-        }
-    }
-    
-    for character in line.chars().rev() {
-        if character.is_digit(10) {
-            last = character.to_digit(10).unwrap() as usize;
-            break;
-        }
-    }
+    let first = line.chars().find(|c| c.is_digit(10)).unwrap();
+    let last = line.chars().rev().find(|c| c.is_digit(10)).unwrap();
 
-    let number = &line[first..last];
-    number.parse::<i32>().unwrap()
+    let combined = format!("{}{}", first, last);
+    combined.parse::<i32>().unwrap_or(0)
 }
 
