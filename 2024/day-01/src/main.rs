7d3be5d98cb9fd381f1d2d119f4db56d27ce7c2a
JulOuellet <julien.ouellet@live.ca>
2024-12-03 22:01:00 -0500

    first part solution

--- 2024/day-01/src/main.rs
+++ 2024/day-01/src/main.rs
@@ -1,13 +1,11 @@
-
-
 fn main() -> std::io::Result<()> {
     let input = std::fs::read_to_string("input.txt")?;
+    let (left, right) = parse_input(&input);
 
-    println!("Part 1: {}", input);
+    println!("Part 1: {}", part_one(left, right));
 
     Ok(())
 }
-
 
 pub fn parse_input(input: &str) -> (Vec<i32>,Vec<i32>) {
     input
@@ -23,4 +21,14 @@ pub fn parse_input(input: &str) -> (Vec<i32>,Vec<i32>) {
                 .map(|v| (v[0], v[1]))
         })
         .unzip()
+}
+
+fn part_one(mut left: Vec<i32>, mut right: Vec<i32>) -> i32 {
+    left.sort();
+    right.sort();
+
+    left.iter()
+        .zip(right.iter())
+        .map(|(l, r)| (l - r).abs())
+        .sum()
 }
