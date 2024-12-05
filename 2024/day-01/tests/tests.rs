633283c6c4f5f781ffcf4e96338e6cc3a2d4d62a
JulOuellet <julien.ouellet@live.ca>
2024-12-04 21:57:58 -0500

    add tests

--- 2024/day-01/tests/tests.rs
+++ 2024/day-01/tests/tests.rs
@@ -1,4 +1,8 @@
-use day_01::parse_input;
+use day_01::{
+    parse_input,
+    part_one,
+    part_two
+};
 
 #[test]
 fn test_parse_input() {
@@ -17,3 +21,28 @@ fn test_parse_input() {
     assert_eq!(left, expected_left, "Left column parsing failed");
     assert_eq!(right, expected_right, "Right column parsing failed");
 }
+
+#[test]
+fn test_part_one() {
+    let left = vec![3, 4, 2, 1, 3, 3];
+    let right = vec![4, 3, 5, 3, 9, 3];
+
+    let expected_dist = 11;
+
+    let dist = part_one(&left, &right);
+
+    assert_eq!(dist, expected_dist, "Part one failed");
+}
+
+#[test]
+fn test_part_two() {
+    let left = vec![3, 4, 2, 1, 3, 3];
+    let right = vec![4, 3, 5, 3, 9, 3];
+
+    let expected_score = 31;
+
+    let score = part_two(&left, &right);
+
+    assert_eq!(score, expected_score, "Part two failed");
+}
+
