0d4718cee9fb4b3a861b6ba47173724a523c3a53
JulOuellet <julien.ouellet@live.ca>
2024-12-05 21:13:19 -0500

    more tests

--- 2024/day-02/tests/tests.rs
+++ 2024/day-02/tests/tests.rs
@@ -1,5 +1,6 @@
 use day_02::{
-    parse_input
+    parse_input,
+    part_one,
 };
 
 #[test]
@@ -21,6 +22,24 @@ fn test_parse_input() {
     ];
 
     let actual = parse_input(input);
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_part_one() {
+    let reports = vec![
+        vec![7, 6, 4, 2, 1],
+        vec![1, 2, 7, 8, 9],
+        vec![9, 7, 6, 2, 1],
+        vec![1, 3, 2, 4, 5],
+        vec![8, 6, 4, 4, 1],
+        vec![1, 3, 6, 7, 9]
+    ];
+
+    let expected = 2;
+
+    let actual = part_one(reports);
 
     assert_eq!(actual, expected);
 }
