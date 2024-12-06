ceac7e241b1c9447993e0c747f19731512c73e6c
JulOuellet <julien.ouellet@live.ca>
2024-12-05 20:37:51 -0500

    add first tests

---
+++ 2024/day-02/tests/tests.rs
@@ -0,0 +1,26 @@
+use day_02::{
+    parse_input
+};
+
+#[test]
+fn test_parse_input() {
+    let input = "7 6 4 2 1
+        1 2 7 8 9
+        9 7 6 2 1
+        1 3 2 4 5
+        8 6 4 4 1
+        1 3 6 7 9";
+
+    let expected = vec![
+        vec![7, 6, 4, 2, 1],
+        vec![1, 2, 7, 8, 9],
+        vec![9, 7, 6, 2, 1],
+        vec![1, 3, 2, 4, 5],
+        vec![8, 6, 4, 4, 1],
+        vec![1, 3, 6, 7, 9]
+    ];
+
+    let actual = parse_input(input);
+
+    assert_eq!(actual, expected);
+}
