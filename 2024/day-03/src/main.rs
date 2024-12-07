c147cd634cf38604eb2ffb7433effb800079c215
JulOuellet <julien.ouellet@live.ca>
2024-12-06 20:46:51 -0500

    working first part

--- 2024/day-03/src/main.rs
+++ 2024/day-03/src/main.rs
@@ -1,3 +1,22 @@
+use regex::Regex;
+
 fn main() {
-    println!("Hello, world!");
+    let input = std::fs::read_to_string("input.txt").unwrap();
+
+    println!("Part one: {}", part_one(&input));
+}
+
+pub fn part_one(input: &str) -> i32 {
+    let regex = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+
+    let multiplications: Vec<(i32, i32)> = regex.captures_iter(input)
+        .filter_map(|cap| {
+            match (cap[1].trim().parse(), cap[2].trim().parse()) {
+                (Ok(x), Ok(y)) => Some((x, y)),
+                _ => None
+            }
+        })
+    .collect();
+
+    multiplications.iter().map(|(x, y)| x * y).sum()
 }
---
+++ 2024/day-03/tests/tests.rs
@@ -0,0 +1,13 @@
+use day_03::{
+    part_one
+};
+
+#[test]
+fn test_part_one() {
+    let input = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
+
+    let expected = 161;
+    let actual = part_one(input);
+
+    assert_eq!(expected, actual);
+}
