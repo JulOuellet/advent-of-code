feaa7b50ae9f722d27ab7b58663d0b7567e802a1
JulOuellet <julien.ouellet@live.ca>
2024-12-09 22:17:37 -0500

    working second part

--- 2024/day-04/src/main.rs
+++ 2024/day-04/src/main.rs
@@ -1,8 +1,11 @@
+use std::collections::HashMap;
+
 fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let grid = parse_input(&input);
 
     println!("Part One: {}", part_one(&grid));
+    println!("Part Two: {}", part_two(&grid));
 }
 
 pub fn parse_input(input: &str) -> Vec<Vec<char>> {
@@ -18,10 +21,10 @@ pub fn parse_input(input: &str) -> Vec<Vec<char>> {
 pub fn part_one(grid: &Vec<Vec<char>>) -> usize {
     let mut xmas_count = 0;
 
-    // Loop on each char
     for (row_index, row) in grid.iter().enumerate() {
         for (col_index, &char) in row.iter().enumerate() {
             if char == 'X' {
+
                 xmas_count += process_node(&grid, row_index, col_index);
             }
         }
@@ -30,7 +33,7 @@ pub fn part_one(grid: &Vec<Vec<char>>) -> usize {
     xmas_count
 }
 
-fn process_node(grid: &Vec<Vec<char>>, row_index: usize, col_index: usize) -> usize {
+fn process_node(grid: &Vec<Vec<char>>, row: usize, col: usize) -> usize {
     let directions = [
         (0, 1),  // Right
         (0, -1), // Left
@@ -45,14 +48,13 @@ fn process_node(grid: &Vec<Vec<char>>, row_index: usize, col_index: usize) -> us
     let mut xmas_count = 0;
 
     for &(delta_row, delta_col) in &directions {
-        if check_xmas(grid, row_index, col_index, delta_row, delta_col) {
+        if check_xmas(grid, row, col, delta_row, delta_col) {
             xmas_count += 1;
         }
     }
 
     xmas_count
 }
-
 
 fn check_xmas(grid: &Vec<Vec<char>>, row: usize, col: usize, delta_row: isize, delta_col: isize) -> bool {
     let word = ['X', 'M', 'A', 'S'];
@@ -70,4 +72,49 @@ fn check_xmas(grid: &Vec<Vec<char>>, row: usize, col: usize, delta_row: isize, d
         }
     }
     true
+}
+
+pub fn part_two(grid: &Vec<Vec<char>>) -> usize {
+    let directions = [
+        [(-1, -1), (1, 1)],
+        [(-1, 1), (1, -1)],
+        [(1, 1), (-1, -1)],
+        [(1, -1), (-1, 1)],
+    ];
+
+    let positions = grid
+        .iter()
+        .enumerate()
+        .flat_map(|(y, row)| {
+            row.iter().enumerate().map(move |(x, &value)| {
+                ((x as i32, y as i32), value)
+            })
+        })
+        .collect::<HashMap<(i32, i32), char>>();
+
+    let mas = ['M', 'S'];
+    let result: usize = positions
+        .iter()
+        .filter(|(_position, value)| **value == 'A')
+        .filter(|(position, _value)| {
+            directions
+                .iter()
+                .map(|ms_positions| {
+                    ms_positions
+                        .iter()
+                        .map(|(dx, dy)| {
+                            let new_pos = (position.0 + dx, position.1 + dy);
+                            positions.get(&new_pos)
+                        })
+                        .enumerate()
+                        .all(|(index, value)| {
+                            mas.get(index) == value
+                        })
+                })
+                .filter(|&b| b)
+                .count()== 2
+        })
+        .count();
+
+    result
 }
