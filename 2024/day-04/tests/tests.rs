9e6b79bfcd5fe41c7ad3b11d160cfa2e30e424f9
JulOuellet <julien.ouellet@live.ca>
2024-12-09 22:19:01 -0500

    tests

--- 2024/day-04/tests/tests.rs
+++ 2024/day-04/tests/tests.rs
@@ -1,6 +1,7 @@
 use day_04::{
     parse_input,
-    part_one
+    part_one,
+    part_two
 };
 
 #[test]
@@ -50,6 +51,28 @@ fn test_part_one() {
 
     let expected = 18;
     let actual = part_one(&input);
+
+    assert_eq!(actual, expected);
+}
+
+#[test]
+fn test_part_two() {
+    let input = vec![
+        vec!['M', 'M', 'M', 'S', 'X', 'X', 'M', 'A', 'S', 'M'],
+        vec!['M', 'S', 'A', 'M', 'X', 'M', 'S', 'M', 'S', 'A'],
+        vec!['A', 'M', 'X', 'S', 'X', 'M', 'A', 'A', 'M', 'M'],
+        vec!['M', 'S', 'A', 'M', 'A', 'S', 'M', 'S', 'M', 'X'],
+        vec!['X', 'M', 'A', 'S', 'A', 'M', 'X', 'A', 'M', 'M'],
+        vec!['X', 'X', 'A', 'M', 'M', 'X', 'X', 'A', 'M', 'A'],
+        vec!['S', 'M', 'S', 'M', 'S', 'A', 'S', 'X', 'S', 'S'],
+        vec!['S', 'A', 'X', 'A', 'M', 'A', 'S', 'A', 'A', 'A'],
+        vec!['M', 'A', 'M', 'M', 'M', 'X', 'M', 'M', 'M', 'M'],
+        vec!['M', 'X', 'M', 'X', 'A', 'X', 'M', 'A', 'S', 'X']
+    ];
+
+
+    let expected = 9;
+    let actual = part_two(&input);
 
     assert_eq!(actual, expected);
 }
